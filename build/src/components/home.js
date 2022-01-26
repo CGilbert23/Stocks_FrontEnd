d5098746d30c435bb8a3325e014c4289ff8812be
Chris Gilbert <christophergilbert20@gmail.com>
2022-01-26 13:32:58 -0500

    new

--- build/src/components/home.js
+++ build/src/components/home.js
@@ -26,7 +26,7 @@ const Home = () => {
     const currentDate = `${mm}/${dd}/${yyyy}`
   
     /*State Management : All Exisitng Data */
-    const {data,setData,isPending,error} = useFetch(`${URL}/stocks`)
+    const {data,setData,isPending,error} = useFetch(`${process.env.URL}/stocks`)
 
         /*State Management: New Vehicle Form */
         const initialFormState = {
@@ -51,7 +51,7 @@ const Home = () => {
           event.preventDefault();
           const newStock = (formData)
           console.log(newStock)
-          const url = `${URL}/stocks`
+          const url = `${process.env.URL}/stocks`
           const requestOptions = {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
@@ -75,7 +75,7 @@ const Home = () => {
        
   
         const handleDelete = (stock_id) => {
-          fetch(`${URL}/stocks/${stock_id}`, {
+          fetch(`${process.env.URL}/stocks/${stock_id}`, {
             method: 'DELETE'
           })
           .then(response => console.log(`Deleted Car, ${stock_id}`))
